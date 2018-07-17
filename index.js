37da54f4d18c3682074b2bed70c3d42277860cfe
cjoannes@tulane.edu <cjoannes@tulane.edu>
2018-07-17 19:41:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function destructivelyAppendKitten(name) { kittens.push('Ralph'); return kittens
 function destructivelyPrependKitten(name) { kittens.unshift('Bob'); return kittens }
 function destructivelyRemoveLastKitten(array) { kittens.pop(); return kittens }
 function destructivelyRemoveFirstKitten(array) { kittens.shift(); return kittens }
-function prependKitten(name) { ['Bob', ...kittens]; return kittens }
+function prependKitten(name) { ['Arnold', ...kittens]; return kittens }
 function appendKitten(name) { console.log(kittens.push("Broom")); return kittens }
 function removeLastKitten(array) { return kittens.slice(0, kittens.length -1) }
 function removeFirstKitten(array) { console.log(kittens.splice(0, 1)); return kittens }
